6749bd9a795370885a54281dbd487d94f1934978
bryantomoregie <bryantomoregie@yahoo.com>
2020-04-17 10:32:53 -0500

    bryants challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,24 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new 
+    @heroine = Heroine.new
+    @powers = Power.all
+  end 
+
+  def create
+    @heroine = Heroine.create({
+      name: params[:heroine][:name],
+      super_name: params[:heroine][:super_name],
+      power_id: params[:heroine][:power],
+    })
+    redirect_to "/heroines/#{@heroine.id}"
+  end 
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+
 end
